0316f53cf7b26fe465ec9d1ba3d7e67bbffba86f
Elrey009 <daniloejike@gmail.com>
2024-10-03 18:20:03 +0100

    modified  files

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,5 +1,7 @@
 import { HiOutlineBell, HiOutlineChatAlt, HiOutlineSearch } from 'react-icons/hi'
-import { Popover, PopoverButton } from '@headlessui/react'
+import { Popover, PopoverButton, PopoverPanel, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import classNames from 'classnames'
 
 export default function Header() {
     return (
@@ -16,9 +18,30 @@ export default function Header() {
                 <Popover className={'relative'}>
                     {({ open }) => (
                         <>
-                            <PopoverButton className={''}>
+                            <PopoverButton
+                                className={classNames(
+                                    open && 'bg-gray-100',
+                                    'p-1.5 rounded-full inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100'
+                                )}
+                            >
                                 <HiOutlineChatAlt fontSize={24} />
                             </PopoverButton>
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-200"
+                                enterFrom="opacity-0 translate-y-1"
+                                enterTo="opacity-100 translate-y-0"
+                                leave="transition ease-in duration-150"
+                                leaveFrom="opacity-100 translate-y-0"
+                                leaveTo="opacity-0 translate-y-1"
+                            >
+                                <PopoverPanel className="absolute right-0 z-10 mt-2.5 transform w-80">
+                                    <div className="bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
+                                        <strong className="text-gray-700 font-medium">Messages</strong>
+                                        <div className="mt-2 py-1 text-sm">This is messages panel.</div>
+                                    </div>
+                                </PopoverPanel>
+                            </Transition>
                         </>
                     )}
                 </Popover>
