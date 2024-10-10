99d82bda462a01f1655abfc20b8ebbd766517ed5
Elrey009 <daniloejike@gmail.com>
2024-10-10 22:43:37 +0100

    added new files

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,16 @@
 // import { Link } from 'react-router-dom'
+import BuyProfileChart from './BuyProfileChart'
 import DashboardStatsGrid from './DashboardStatsGrid'
+import TransactionChart from './TransactionChart'
 
 export default function Dashboard() {
     return (
-        <div className="flex gap-4">
+        <div className="flex flex-col gap-4">
             <DashboardStatsGrid />
+            <div className="flex w-full gap-4">
+                <TransactionChart />
+                <BuyProfileChart />
+            </div>
         </div>
     )
 }
