8ad16153905c6fe0d2ac00f8c7bfbb4863cd5b5b
Elrey009 <daniloejike@gmail.com>
2024-10-11 17:22:55 +0100

    added and modified files

---
+++ src/components/utils/popularProduct.js
@@ -0,0 +1,51 @@
+import macBookPro from '../../assets/mac-book.jpg'
+import galaxy from '../../assets/galaxy-buds.jpg'
+import asus from '../../assets/asus.jpg'
+import LG from '../../assets/lg.avif'
+import appleTouchpad from '../../assets/apple-touch-pad.png'
+import nothingBuds from '../../assets/nothing.jpg'
+
+export const popularProducts = [
+    {
+        id: '3432',
+        product_name: 'Macbook M1 Pro 14"',
+        product_thumbnail: macBookPro,
+        product_price: '$1499.00',
+        product_stock: 341
+    },
+    {
+        id: '7633',
+        product_name: 'Samsung Galaxy Buds 2',
+        product_thumbnail: galaxy,
+        product_price: '$399.00',
+        product_stock: 24
+    },
+    {
+        id: '6534',
+        product_name: 'Asus Zenbook Pro',
+        product_thumbnail: asus,
+        product_price: '$899.00',
+        product_stock: 56
+    },
+    {
+        id: '9234',
+        product_name: 'LG Flex Canvas',
+        product_thumbnail: LG,
+        product_price: '$499.00',
+        product_stock: 98
+    },
+    {
+        id: '4314',
+        product_name: 'Apple Magic Touchpad',
+        product_thumbnail: appleTouchpad,
+        product_price: '$699.00',
+        product_stock: 0
+    },
+    {
+        id: '4342',
+        product_name: 'Nothing Earbuds One',
+        product_thumbnail: nothingBuds,
+        product_price: '$399.00',
+        product_stock: 453
+    }
+]
